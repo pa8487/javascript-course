ae1bebf7507d509e2a46a39ea97d395b45b5d270
Parth Patel <admin@Parths-MacBook-Pro.local>
2022-09-22 18:31:56 -0700

    Complete DOM Manipulation

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let btnAgain = document.querySelector('.again');
+btnAgain.addEventListener('click', (e) => {
+    console.log(e);
+});
+
+document.addEventListener('keydown', (e) => {
+    console.log(e);
+});
